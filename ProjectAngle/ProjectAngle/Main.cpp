4cd6308c507c69b80982be9fa476e1bc3f8843c8
UltGuardian <alj@hushmail.me>
2015-03-10 17:30:18 -0700

    The bullet is not moving properly

--- ProjectAngle/ProjectAngle/Main.cpp
+++ ProjectAngle/ProjectAngle/Main.cpp
@@ -102,6 +102,15 @@ void initPlayer(){
 	PLAYER.SPRITE.setTexture(PLAYER_NORTH_TEXTURE);
 	GAME_BOARD[PLAYER.LOCATION_Y][PLAYER.LOCATION_X].ACTOR = &PLAYER;
 }
+
+bool isValidTile(int x, int y){
+	if (x >= 0 && x < BOARD_SIZE && y >= 0 && y < BOARD_SIZE){
+		return true;
+	}
+	else{
+		return false;
+	}
+}
 void makeActor(Tile *tile,int x,int y,Team team,Direction direction){
 	tile->ACTOR = new Actor();
 	tile->ACTOR->LOCATION_X = x;
@@ -140,7 +149,7 @@ void makeActor(Tile *tile,int x,int y,Team team,Direction direction){
 }
 
 void moveActor(Actor *actor,Tile tile){
-	cout << "Moving actor to " << tile.LOCATION_X << ", " << tile.LOCATION_Y << endl;
+	//cout << "Moving actor to " << tile.LOCATION_X << ", " << tile.LOCATION_Y << endl;
 	tile.ACTOR = actor;
 	tile.ACTOR->LOCATION_X = tile.LOCATION_X;
 	tile.ACTOR->LOCATION_Y = tile.LOCATION_Y;
@@ -180,15 +189,15 @@ void updateActorTexture(Actor *actor){
 
 bool isValidMove(int y,int x){
 	if (x < 0 || x >= BOARD_SIZE || y < 0 || y >= BOARD_SIZE){
-		cout << "Not a valid location. " << x << " " << y << endl;
+		//cout << "Not a valid location. " << x << " " << y << endl;
 		return false;
 	}
 	else  if (GAME_BOARD[y][x].ACTOR != nullptr || GAME_BOARD[y][x].BULLET != nullptr || GAME_BOARD[y][x].WALL != nullptr){
-		cout << "The tile is filled. " << GAME_BOARD[y][x].LOCATION_X << " " << GAME_BOARD[y][x].LOCATION_Y << endl;
+		//cout << "The tile is filled. " << GAME_BOARD[y][x].LOCATION_X << " " << GAME_BOARD[y][x].LOCATION_Y << endl;
 		return false;
 	}
 	else{
-		cout << "That's a good move.\n";
+		//cout << "That's a good move.\n";
 		return true;
 	}
 }
@@ -202,26 +211,37 @@ void makeBullet(Tile *tile, int x, int y, Team team, Direction direction){
 	if (team == BLUE){
 		if (direction == NORTH || direction == SOUTH){
 			tile->BULLET->SPRITE.setTexture(PLAYER_BULLET_VERTICAL_TEXTURE);
+			tile->BULLET->SPRITE.setPosition(x*TILE_SIZE + 15, y*TILE_SIZE+5);
 		}
 		else if (direction == WEST || direction == EAST){
 			tile->BULLET->SPRITE.setTexture(PLAYER_BULLET_HORIZONTAL_TEXTURE);
+			tile->BULLET->SPRITE.setPosition(x*TILE_SIZE+5, y*TILE_SIZE + 15);
 		}
 	}
 	else if (team == RED){
 		if (direction == NORTH || direction == SOUTH){
 			tile->BULLET->SPRITE.setTexture(ENEMY_BULLET_VERTICAL_TEXTURE);
+			tile->BULLET->SPRITE.setPosition(x*TILE_SIZE + 15, y*TILE_SIZE+5);
 		}
 		else if (direction == WEST || direction == EAST){
 			tile->BULLET->SPRITE.setTexture(ENEMY_BULLET_HORIZONTAL_TEXTURE);
+			tile->BULLET->SPRITE.setPosition(x*TILE_SIZE+5, y*TILE_SIZE + 15);
 		}
 	}
-	tile->BULLET->SPRITE.setPosition(x * TILE_SIZE, y * TILE_SIZE);
+	tile->BULLET->hasMoved = false;
 }
 
 void moveBullet(Tile *tileFrom, Tile *tileTo){
 	tileTo->BULLET = tileFrom->BULLET;
 	tileFrom->BULLET = nullptr;
 	tileTo->BULLET->SPRITE.setPosition(tileTo->LOCATION_X*TILE_SIZE, tileTo->LOCATION_Y*TILE_SIZE);
+	if (tileTo->BULLET->DIRECTION == NORTH || tileTo->BULLET->DIRECTION == SOUTH){
+		tileTo->BULLET->SPRITE.setPosition(tileTo->LOCATION_X*TILE_SIZE + 15, tileTo->LOCATION_Y*TILE_SIZE + 5);
+	}
+	else if (tileTo->BULLET->DIRECTION == EAST || tileTo->BULLET->DIRECTION == WEST){
+		tileTo->BULLET->SPRITE.setPosition(tileTo->LOCATION_X*TILE_SIZE + 5, tileTo->LOCATION_Y*TILE_SIZE + 15);
+	}
+	cout << "New bullet coordinates: " << tileTo->LOCATION_X << ", " << tileTo->LOCATION_Y << endl;
 }
 
 void removeBullet(Bullet *bullet){
@@ -263,15 +283,15 @@ void isValidBulletMove(int y,int x,Bullet *bullet){
 		}
 	}
 	else{
-		cout << x << " " << y << " " << BOARD_SIZE << endl;
 		removeBullet(bullet);
 	}
 }
 void moveBullets(){
 	for (int currentRow = 0; currentRow < BOARD_SIZE; currentRow++){
-		for (int currentCol = 0; currentCol < BOARD_SIZE; currentRow++){
+		for (int currentCol = 0; currentCol < BOARD_SIZE; currentCol++){
 			if (GAME_BOARD[currentRow][currentCol].BULLET != nullptr){
 				if (!GAME_BOARD[currentRow][currentCol].BULLET->hasMoved){
+					cout << "Moving a bullet\n";
 					GAME_BOARD[currentRow][currentCol].BULLET->hasMoved = true;
 					if (GAME_BOARD[currentRow][currentCol].BULLET->DIRECTION == NORTH){
 						isValidBulletMove(currentRow - 1, currentCol,GAME_BOARD[currentRow][currentCol].BULLET);
@@ -292,31 +312,25 @@ void moveBullets(){
 }
 
 void fireBullet(){
-	GAME_BOARD[PLAYER.LOCATION_Y][PLAYER.LOCATION_X].BULLET = new Bullet();
-	Bullet *bullet = GAME_BOARD[PLAYER.LOCATION_Y][PLAYER.LOCATION_X].BULLET;
-	cout << "bullet " << GAME_BOARD[PLAYER.LOCATION_Y][PLAYER.LOCATION_X].BULLET << endl;
-	bullet->LOCATION_X = PLAYER.LOCATION_X;
-	bullet->LOCATION_Y = PLAYER.LOCATION_Y;
-	bullet->TEAM = BLUE;
-	bullet->DIRECTION = PLAYER.DIRECTION;
-	if (bullet->DIRECTION == NORTH || bullet->DIRECTION == SOUTH){
-		bullet->SPRITE.setTexture(PLAYER_BULLET_VERTICAL_TEXTURE);
-	}
-	else if (bullet->DIRECTION == EAST || bullet->DIRECTION == WEST){
-		bullet->SPRITE.setTexture(PLAYER_BULLET_HORIZONTAL_TEXTURE);
-	}
-	bullet->SPRITE.setPosition(bullet->LOCATION_X * TILE_SIZE, bullet->LOCATION_Y * TILE_SIZE);
 	if (PLAYER.DIRECTION == NORTH){
-		isValidBulletMove(PLAYER.LOCATION_Y - 1, PLAYER.LOCATION_X, GAME_BOARD[PLAYER.LOCATION_Y][PLAYER.LOCATION_X].BULLET);
+		if (isValidTile(PLAYER.LOCATION_X, PLAYER.LOCATION_Y - 1)){
+			makeBullet(&GAME_BOARD[PLAYER.LOCATION_Y - 1][PLAYER.LOCATION_X], PLAYER.LOCATION_X, PLAYER.LOCATION_Y - 1, BLUE, NORTH);
+		}
 	}
 	else if (PLAYER.DIRECTION == EAST){
-		isValidBulletMove(PLAYER.LOCATION_Y - 1, PLAYER.LOCATION_X, GAME_BOARD[PLAYER.LOCATION_Y][PLAYER.LOCATION_X].BULLET);
+		if (isValidTile(PLAYER.LOCATION_X + 1, PLAYER.LOCATION_Y)){
+			makeBullet(&GAME_BOARD[PLAYER.LOCATION_Y][PLAYER.LOCATION_X + 1], PLAYER.LOCATION_X + 1, PLAYER.LOCATION_Y, BLUE, EAST);
+		}
 	}
 	else if (PLAYER.DIRECTION == SOUTH){
-		isValidBulletMove(PLAYER.LOCATION_Y - 1, PLAYER.LOCATION_X, GAME_BOARD[PLAYER.LOCATION_Y][PLAYER.LOCATION_X].BULLET);
+		if (isValidTile(PLAYER.LOCATION_X, PLAYER.LOCATION_Y + 1)){
+			makeBullet(&GAME_BOARD[PLAYER.LOCATION_X + 1][PLAYER.LOCATION_X], PLAYER.LOCATION_X, PLAYER.LOCATION_Y + 1, BLUE, SOUTH);
+		}
 	}
 	else if (PLAYER.DIRECTION == WEST){
-		isValidBulletMove(PLAYER.LOCATION_Y - 1, PLAYER.LOCATION_X, GAME_BOARD[PLAYER.LOCATION_Y][PLAYER.LOCATION_X].BULLET);
+		if (isValidTile(PLAYER.LOCATION_X - 1, PLAYER.LOCATION_Y)){
+			makeBullet(&GAME_BOARD[PLAYER.LOCATION_Y][PLAYER.LOCATION_X - 1], PLAYER.LOCATION_X - 1, PLAYER.LOCATION_Y, BLUE, WEST);
+		}
 	}
 }
 
@@ -357,7 +371,7 @@ void checkKeyboardInputs(){
 			moveActor(&PLAYER, GAME_BOARD[PLAYER.LOCATION_Y][PLAYER.LOCATION_X-1]);
 		}
 		else{
-			cout << "Not a valid move.\n";
+			//cout << "Not a valid move.\n";
 		}
 	}
 	else if (sf::Keyboard::isKeyPressed(sf::Keyboard::W)){
@@ -367,7 +381,7 @@ void checkKeyboardInputs(){
 			moveActor(&PLAYER, GAME_BOARD[PLAYER.LOCATION_Y - 1][PLAYER.LOCATION_X]);
 		}
 		else{
-			cout << "Not a valid move.\n";
+			//cout << "Not a valid move.\n";
 		}
 	}
 	else if (sf::Keyboard::isKeyPressed(sf::Keyboard::S)){
@@ -377,7 +391,7 @@ void checkKeyboardInputs(){
 			moveActor(&PLAYER, GAME_BOARD[PLAYER.LOCATION_Y + 1][PLAYER.LOCATION_X]);
 		}
 		else{
-			cout << "Not a valid move.\n";
+			//cout << "Not a valid move.\n";
 		}
 	}
 	else if (sf::Keyboard::isKeyPressed(sf::Keyboard::D)){
@@ -387,7 +401,7 @@ void checkKeyboardInputs(){
 			moveActor(&PLAYER, GAME_BOARD[PLAYER.LOCATION_Y][PLAYER.LOCATION_X + 1]);
 		}
 		else{
-			cout << "Not a valid move.\n";
+			//cout << "Not a valid move.\n";
 		}
 	}
 	else if (sf::Keyboard::isKeyPressed(sf::Keyboard::Space)){
@@ -404,8 +418,7 @@ void drawEverything(){
 				WINDOW.draw(GAME_BOARD[currentRow][currentCol].ACTOR->SPRITE);
 			}
 			else if (GAME_BOARD[currentRow][currentCol].BULLET != nullptr){
-				GAME_BOARD[currentRow][currentCol].BULLET->SPRITE.setTexture(PLAYER_BULLET_VERTICAL_TEXTURE);
-				WINDOW.draw(GAME_BOARD[currentRow][currentCol].BULLET->SPRITE);//Error occurs here
+				//WINDOW.draw(GAME_BOARD[currentRow][currentCol].BULLET->SPRITE);//Error occurs here
 			}
 			else if (GAME_BOARD[currentRow][currentCol].WALL != nullptr){
 				WINDOW.draw(GAME_BOARD[currentRow][currentCol].WALL->SPRITE);
@@ -437,6 +450,7 @@ int main()
 			deAllocateEverything();
 			return 0;
 		}
+		moveBullets();
 		while (!hasInput){
 			deltaTime = CLOCK.getElapsedTime().asMilliseconds();
 			if (deltaTime >= 75){
